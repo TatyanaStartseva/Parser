f6400b6a5d9cfcee7698bc363fb81d1816473ad2
Startseva Tatyana <TatyanaStartseva2020@yandex.ru>
2024-03-24 21:24:43 +0300

    Corrected  'Messages' and added 'env'

--- server_save.py
+++ server_save.py
@@ -1,12 +1,16 @@
 import asyncio
+import os
 import sys
 from aiohttp import web
 from pymongo import MongoClient
 from retry import retry
+from dotenv import load_dotenv
+load_dotenv()
+MONGODB_URI = os.getenv("MONGODB_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+cluster = MongoClient(MONGODB_URI)
+db = cluster[DATABASE_NAME]
 
-
-cluster = MongoClient("mongodb+srv://tatyanastartseva2020:IIr08PTqUBbyK0Jq@parserdb.gwhsqxg.mongodb.net/Parserdb")
-db = cluster["Parserdb"]
 
 async def retry(func_name, *args, **kwargs):
     async def _wrapper():
@@ -17,16 +21,11 @@ async def retry(func_name, *args, **kwargs):
             except Exception as e:
                 print(f"Произошла ошибка: {e}. Повторная попытка...")
                 await asyncio.sleep(1)
-        else:
-            raise Exception("Сбой после нескольких попыток")
     await _wrapper()
 
 async def insert_or_update_one(collection, query, update=None):
     try:
-        if update is None:
-            return
-        result = collection.update_one(query, {'$set': update}, upsert=True)
-        return result
+        return collection.update_one(query, {'$set': update}, upsert=True)
     except Exception as e:
         raise e
 
@@ -35,29 +34,28 @@ async def Users(data):
         collection = db["Users"]
         for key in data['accounts']:
             exist_user = collection.find_one({'user_id': key})
+            accounts_info = data['accounts'][key]['info']
             username = None
-            if data['accounts'][key]['info'].get('username') is not None:
-                username = data['accounts'][key]['info'].get('username').lower()
+            if accounts_info.get('username') is not None:
+                username = accounts_info.get('username').lower()
             update = {
-                'user_id': key,
-                'username': username,
-                'first_name': data['accounts'][key]['info'].get('first_name'),
-                'last_name': data['accounts'][key]['info'].get('last_name'),
-                'last_online': data['accounts'][key]['info'].get('last_online'),
-                'premium': data['accounts'][key]['info'].get('premium'),
-                'phone': data['accounts'][key]['info'].get('phone'),
-                'image': data['accounts'][key]['info'].get('image'),
-                'past_first_name': None,
-                'past_last_name':None
-            }
+                    'user_id': key,
+                    'username': username,
+                    'first_name': accounts_info.get('first_name'),
+                    'last_name': accounts_info.get('last_name'),
+                    'last_online': accounts_info.get('last_online'),
+                    'premium': accounts_info.get('premium'),
+                    'phone': accounts_info.get('phone'),
+                    'image': accounts_info.get('image'),
+                    'past_first_name': None,
+                    'past_last_name': None
+                }
             if exist_user is not None:
-                if exist_user.get('first_name') != update['first_name']:
+                if exist_user.get('first_name') != update.get('first_name'):
                     update['past_first_name'] = exist_user.get('first_name')
-                if exist_user.get('last_name') != update['last_name']:
+                if exist_user.get('last_name') != update.get('last_name'):
                     update['past_last_name'] = exist_user.get('last_name')
-
-            query = {'user_id': key}
-            await retry(insert_or_update_one, collection, query, update)
+            await retry(insert_or_update_one, collection, {'user_id': key}, update)
     except Exception as e:
         print(f'Error: {e}')
         print(sys.exc_info())
@@ -68,12 +66,11 @@ async def Messages(user_data):
         for key in user_data['accounts']:
             for key_chat in user_data['chats']:
                 for messages in user_data['accounts'][key]['chats']:
-                    for i in range(len(user_data['accounts'][key]['chats'][messages])):
-                        message_id = user_data['accounts'][key]['chats'][messages][i]['message_id']
-                        text = user_data['accounts'][key]['chats'][messages][i]['text']
-                        exist_message = collection.find_one({'message_id': message_id})
-                        if exist_message is None:
-                            await retry(insert_or_update_one, collection, {'message_id': message_id, 'user_id': key, 'chat_id': key_chat, 'text': text})
+                    for message_data in user_data['accounts'][key]['chats'][messages]:
+                        message_id = message_data['message_id']
+                        text = message_data['text']
+                        await retry(insert_or_update_one, collection,{'message_id':message_id} ,{'message_id': message_id, 'user_id': key, 'chat_id': key_chat, 'text': text})
+
     except Exception as e:
         print(f'Error: {e}')
         print(sys.exc_info())
@@ -82,20 +79,20 @@ async def Chats(data):
     try:
         collection = db["Chats"]
         for key in data['chats']:
+            chats_key = data['chats'][key]
             exist_chat = collection.find_one({'chat_id': key})
             update = {
-                'chat_id': key,
-                'username': data['chats'][key].get('username'),
-                'title': data['chats'][key].get('title'),
-                'last_online': data['chats'][key].get('last_online'),
-                'past_title': None
-            }
+                    'chat_id': key,
+                    'username': chats_key.get('username'),
+                    'title': chats_key.get('title'),
+                    'last_online': chats_key.get('last_online')
+                }
             if exist_chat is not None:
-                if exist_chat.get('title') != update['title']:
+                if exist_chat.get('title') != chats_key.get('title'):
                     update['past_title'] = exist_chat.get('title')
-
-            query = {'chat_id': key}
-            await retry(insert_or_update_one, collection, query, update)
+                if exist_chat.get("last_online") != chats_key.get('last_online'):
+                    update['last_online'] = chats_key.get('last_online')
+                await retry(insert_or_update_one, collection, {'chat_id': key}, update)
     except Exception as e:
         print(f'Error: {e}')
         print(sys.exc_info())
