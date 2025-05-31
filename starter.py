f056dc663ab8a5f34cd17d713f5f808144d535a4
TatyanaStartseva <TatyanaStartseva2020@yandex.ru>
2025-05-31 13:35:22 +0300

    изменены файлы сессии и starter.py

--- starter.py
+++ starter.py
@@ -1,18 +1,26 @@
 import subprocess
 import json
+import os
 
-# Чтение данных из файла sessions.json
-with open("sessions.json", "r") as file:
+base_dir = os.path.dirname(os.path.abspath(__file__))  # папка со скриптом
+sessions_path = os.path.join(base_dir, "sessions.json")
+parser_path = os.path.join(base_dir, "parser.py")
+
+with open(sessions_path, "r") as file:
     sessions = json.load(file)
 
-# Проход по каждому объекту и вызов команды в консоли
 for session in sessions:
     api_id = session["api_id"]
     api_hash = session["api_hash"]
     session_value = session["session_value"]
 
-    # Формирование строки для вызова команды
-    command = f"pm2 start parser.py --interpreter=python3 --name {api_id} -- {api_id} {api_hash} {session_value}"
-
-    # Вызов команды в консоли
-    subprocess.run(command, shell=True)
+    command = [
+        "pm2", "start", parser_path,
+        "--interpreter", "python3",
+        "--name", str(api_id),
+        "--",
+        str(api_id),
+        str(api_hash),
+        str(session_value)
+    ]
+    subprocess.run(command, shell=False)
